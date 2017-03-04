d90258ea85e7038bb35bb2eae5bcfa20a4d33239
DanielRoszhart <roszhart2016@gmail.com>
2017-03-04 16:04:47 -0600

    Layout design for the Bible Challenge App

---
+++ app/src/main/java/com/example/dan/biblechallenge/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dan.biblechallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+    }
+}
